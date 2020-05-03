d109ede6f1d3f0ef3715f860067f0b72603d42ac
chenfei <969154227@qq.com>
2020-05-03 20:45:55 +0800

    demo

---
+++ zuul-demo/src/main/java/com/chenfei/zuuldemo/ZuulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chenfei.zuuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulDemoApplication.class, args);
+	}
+
+}
---
+++ zuul-demo/src/test/java/com/chenfei/zuuldemo/ZuulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenfei.zuuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
