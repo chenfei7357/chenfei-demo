cc5d0004e773e42896030b8afc89906791bba721
chenfei <969154227@qq.com>
2020-05-31 23:43:13 +0800

    demo

---
+++ eureka-server-demo/src/test/java/com/chenfei/eureka/server/demo/EurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenfei.eureka.server.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
