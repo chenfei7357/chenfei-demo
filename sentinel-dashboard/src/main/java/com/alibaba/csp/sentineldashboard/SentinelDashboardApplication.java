afaf37c3d7b03be1adce0a1d94aaec83a6822769
chenfei <969154227@qq.com>
2020-06-22 14:16:04 +0800

    新增针对官方控制台改造后的代码【基于官方源码改造】

---
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentineldashboard/SentinelDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.csp.sentineldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelDashboardApplication.class, args);
+	}
+
+}
---
+++ sentinel-dashboard/src/test/java/com/alibaba/csp/sentineldashboard/SentinelDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.csp.sentineldashboard;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SentinelDashboardApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
