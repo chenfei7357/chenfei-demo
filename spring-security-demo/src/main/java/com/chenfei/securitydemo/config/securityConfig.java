bcb2cc20e533a3a2ce290572d167682b47dd7599
chenfei <969154227@qq.com>
2020-07-15 23:35:15 +0800

    demo

--- spring-security-demo/src/main/java/com/chenfei/securitydemo/config/securityConfig.java
+++ spring-security-demo/src/main/java/com/chenfei/securitydemo/config/securityConfig.java
@@ -1,15 +1,20 @@
 package com.chenfei.securitydemo.config;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.PrintWriter;
 
 @Configuration
 public class securityConfig  extends WebSecurityConfigurerAdapter {
@@ -19,21 +24,21 @@ public class securityConfig  extends WebSecurityConfigurerAdapter {
 	 * 密码加密管理实例
 	 * @return
 	 */
-//	@Bean
-//	PasswordEncoder passwordEncoder() {
-//		return NoOpPasswordEncoder.getInstance();
-//	}
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 
 	/**
 	 * 角色继承
 	 * @return
 	 */
-//	@Bean
-//	RoleHierarchy roleHierarchy() {
-//		RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
-//		hierarchy.setHierarchy("ROLE_admin > ROLE_user");
-//		return hierarchy;
-//	}
+	@Bean
+	RoleHierarchy roleHierarchy() {
+		RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+		hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+		return hierarchy;
+	}
 
 	/**
 	 * 用户名、权限相关配置
@@ -44,11 +49,11 @@ public class securityConfig  extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 				.withUser("admin")
-				.password("{noop}123")
+				.password("123")
 				.roles("ADMIN")
 				.and()
 				.withUser("user")
-				.password("{noop}123")
+				.password("123")
 				.roles("USER");
 
 	}
@@ -77,9 +82,60 @@ public class securityConfig  extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated()
 				.and()
 				.formLogin()
+				.loginProcessingUrl("/doLogin")//设置登陆接口地址
+				.permitAll()
+				.successHandler((req, res, authentication) -> {
+					res.setContentType("application/json;charset=utf-8");
+					PrintWriter writer = res.getWriter();
+					writer.write(JSON.toJSONString(authentication));
+					writer.flush();
+					writer.close();
+				})
+				.failureHandler((req, res, e) -> {
+					res.setContentType("application/json;charset=utf-8");
+					PrintWriter writer = res.getWriter();
+					writer.write(JSON.toJSONString(this.dealException(e)));
+					writer.flush();
+					writer.close();
+				})
 				.and()
-				.httpBasic();
+				.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessHandler((req, res, authentication) -> {
+					res.setContentType("application/json;charset=utf-8");
+					PrintWriter writer = res.getWriter();
+					writer.write(JSON.toJSONString("注销成功"));
+					writer.flush();
+					writer.close();
+				})
+				.and()
+				.csrf()
+				.disable()
+				.exceptionHandling()
+				.authenticationEntryPoint((req, res, e) -> {
+					res.setContentType("application/json;charset=utf-8");
+					PrintWriter writer = res.getWriter();
+					writer.write(JSON.toJSONString("未查到用户信息，还未登陆"));
+					writer.flush();
+					writer.close();
+				})
+		;
+	}
 
-
+	private String dealException(AuthenticationException e) {
+		String msg="";
+		if (e instanceof LockedException) {
+			msg="账户被锁定，请联系管理员!";
+		} else if (e instanceof CredentialsExpiredException) {
+			msg="密码过期，请联系管理员!";
+		} else if (e instanceof AccountExpiredException) {
+			msg="账户过期，请联系管理员!";
+		} else if (e instanceof DisabledException) {
+			msg="账户被禁用，请联系管理员!";
+		} else if (e instanceof BadCredentialsException) {
+			msg="用户名或者密码输入错误，请重新输入!";
+		}
+		System.out.println(e.getMessage());
+		return "登陆异常，错误信息为:"+msg;
 	}
 }
--- spring-security-demo/src/main/java/com/chenfei/securitydemo/web/Demo.java
+++ spring-security-demo/src/main/java/com/chenfei/securitydemo/web/Demo.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/demo")
 public class Demo {
 
 
