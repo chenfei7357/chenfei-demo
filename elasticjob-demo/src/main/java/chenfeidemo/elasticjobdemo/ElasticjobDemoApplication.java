3f0169b1fdc90896e8f7db8b586bdf5549bb66a7
chenfei <969154227@qq.com>
2020-08-12 18:13:23 +0800

    新增新版本的elasticjob

---
+++ elasticjob-demo/src/main/java/chenfeidemo/elasticjobdemo/ElasticjobDemoApplication.java
@@ -0,0 +1,13 @@
+package chenfeidemo.elasticjobdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticjobDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticjobDemoApplication.class, args);
+	}
+
+}
